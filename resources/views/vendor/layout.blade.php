ff124afdd2991c0664a8215162fec7e2605e5d1c
gaganshukla913 <gagan.s@chapter247.com>
2021-05-05 18:34:45 +0530

    template install on laravel, auth install

---
+++ resources/views/vendor/layout.blade.php
@@ -0,0 +1,14 @@
+@include('vendor.layout.header')
+<main>
+    <section>
+        <div class="container">
+            @yield('content')
+        </div>
+    </section>
+</main>
+@include('vendor.layout.footer')
+
+
+
+
+
---
+++ resources/views/vendor/welcome.blade.php
@@ -0,0 +1,57 @@
+@include('vendor.layout.header')
+
+  <main id="main">
+    <!-- ======= About Section ======= -->
+    @include('vendor.aboutus.aboutus')
+    <!-- End About Section -->
+
+    <!-- ======= Values Section ======= -->
+    @include('vendor.ourValues.ourvalue')
+    <!-- End Values Section -->
+
+    <!-- ======= Counts Section ======= -->
+    @include('vendor.counts.count')
+    <!-- End Counts Section -->
+
+    <!-- ======= Features Section ======= -->
+    @include('vendor.feature.feature')
+    <!-- End Features Section -->
+
+    <!-- ======= Services Section ======= -->
+    @include('vendor.services.service')
+    <!-- End Services Section -->
+
+    <!-- ======= Pricing Section ======= -->
+    @include('vendor.pricing.pricing')
+    <!-- End Pricing Section -->
+
+    <!-- ======= F.A.Q Section ======= -->
+    @include('vendor.FAQs.faq')
+    <!-- End F.A.Q Section -->
+
+    <!-- ======= Portfolio Section ======= -->
+    @include('vendor.portfolio.portfolio')
+    <!-- End Portfolio Section -->
+
+    <!-- ======= Testimonials Section ======= -->
+    @include('vendor.testimonial.testimonial')
+    <!-- End Testimonials Section -->
+
+    <!-- ======= Team Section ======= -->
+    @include('vendor.team.team')
+    <!-- End Team Section -->
+
+    <!-- ======= Clients Section ======= -->
+    @include('vendor.ourclient.ourclient')
+    <!-- End Clients Section -->
+
+    <!-- ======= Recent Blog Posts Section ======= -->
+    @include('vendor.blog.recentblog')
+    <!-- End Recent Blog Posts Section -->
+
+    <!-- ======= Contact Section ======= -->
+    @include('vendor.contactus.contact')
+    <!-- End Contact Section -->
+
+  </main><!-- End #main -->
+@include('vendor.layout.footer')
